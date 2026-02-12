913b7d475b63017733ea25e4c505abd0ac1b96bd
Hyojin <hynn0254@gmail.com>
2026-02-12 19:44:04 +0900

    add comparing codes and results

---
+++ compare_up_down.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import butter, filtfilt
+
+def butter_bandpass(lowcut, highcut, data, fs, order):
+    b, a = butter(order, [lowcut, highcut], btype='bandpass', fs=fs)
+    y = filtfilt(b, a, data)
+    return y
+
+name = 'JHJ'
+date = '20260212'
+
+file_name_st = f'{date}_{name}_steady'
+pzt_signal_st = pd.read_csv(f'BP-piezo/data/{file_name_st}.csv')['1'].to_numpy()[1:]
+pzt_signal_st = np.asarray(pzt_signal_st, dtype=np.float64)
+filtered_st = butter_bandpass(0.3, 5.0, pzt_signal_st, 400, 3)
+
+file_name_ex = f'{date}_{name}_exercise'
+pzt_signal_ex = pd.read_csv(f'BP-piezo/data/{file_name_ex}.csv')['1'].to_numpy()[1:]
+pzt_signal_ex = np.asarray(pzt_signal_ex, dtype=np.float64)
+filtered_ex = butter_bandpass(0.3, 5.0, pzt_signal_ex, 400, 3)
+
+
+plt.figure(figsize=(12, 5))
+plt.plot(filtered_st, label='steady')
+plt.plot(filtered_ex, label='exercise')
+plt.legend()
+# plt.subplot(1, 2, 1)
+# # plt.plot(pzt_signal_st)
+# plt.plot(filtered_st)
+
+# plt.subplot(1, 2, 2)
+# # plt.plot(pzt_signal_ex)
+# plt.plot(filtered_ex)
+plt.show()
--- wavelet_transform_test.py
+++ wavelet_transform_test.py
@@ -1,22 +1,36 @@
 import pywt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.signal import butter, filtfilt
+
+# Make sine signal for example
+fs = 1000
+duration = 1.0
+freq = 5
+
+t = np.linspace(0, duration, int(fs*duration), endpoint=False)
+y = np.sin(2 * np.pi * freq * t)
+
+# Add noise
+np.random.seed(0)   # Fix noise
+noise_std = 0.2
+noise = np.random.normal(0, noise_std, size=len(y))
+
+y_noise = y + noise
 
 
-def butter_bandpass(lowcut, highcut, data, fs, order):
-    b, a = butter(order, [lowcut, highcut], btype='bandpass', fs=fs)
-    y = filtfilt(b, a, data)
-    return y
+######## Using pywt library
+# # Check coif wave list
+# a = pywt.wavelist('coif')
+# print(a)
 
-# Read PZT sensor signal
-file_name = '20260211_JHJ'
-pzt_signal = pd.read_csv(f'BP-piezo/data/{file_name}.csv')['1'].to_numpy()[1:]
-pzt_signal = np.asarray(pzt_signal, dtype=np.float64)
+# # Use coif9(mother wavelet) with decomposition level = 4
+# wavelet = pywt.Wavelet('coif9') 
+# print(wavelet)
 
-# fs = 1/0.0025s = 400 Hz
-# pzt_denoised = butter_bandpass(0.3, 5.0, pzt_signal, 400, 3)
+# # phi : wavelet function 
+# # psi : scaling function 
+# phi, psi, x = wavelet.wavefun(level=4)
+
 
 ######## Implement paper
 
@@ -26,7 +40,7 @@ pzt_signal = np.asarray(pzt_signal, dtype=np.float64)
 # multi-level 2D -> pywt.wavedec2  
 # coeffs = cA_n, cD_n, cD_n-1, ..., cD1
 # cA : approximation coefficients(low freq), cD : detail coefficients(high freq)
-coeffs = pywt.wavedec(data=pzt_signal, wavelet='coif9', level=4)
+coeffs = pywt.wavedec(data=y_noise, wavelet='coif9', level=4)
 
 cA4 = coeffs[0] 
 cDs = coeffs[1:]
@@ -34,9 +48,9 @@ cD1 = cDs[-1]
 print(coeffs)
 
 ## Thresholding : The universal threshold (Donoho–Johnstone)
-N = len(pzt_signal)
+N = len(y_noise)
 
-sigma = 1.482579 * np.median(np.abs(pzt_signal - np.median(pzt_signal)))
+sigma = 1.482579 * np.median(np.abs(y_noise - np.median(y_noise)))
 # sigma = np.median(np.abs(cD1))  # if cD1 is almost zero-mean noise
 
 threshold = sigma * np.sqrt(2 * np.log(N))
@@ -46,22 +60,26 @@ cDs_thresh = [pywt.threshold(cD, threshold, mode='hard') for cD in cDs]
 coeffs_denoised = [cA4] + cDs_thresh
 # single level inverse -> pywt.idwt
 # multi-level inverse(reconstruction) -> pywt.waverec
-# pzt_denoised_wv = pywt.waverec(coeffs_denoised, wavelet='coif9')
-pzt_denoised = butter_bandpass(0.3, 5.0, pzt_signal, 400, 3)
+y_denoised = pywt.waverec(coeffs_denoised, wavelet='coif9')
+
 
 ######## Plot
+import matplotlib.pyplot as plt
+
 plt.figure(figsize=(12, 5))
 plt.subplot(1, 3, 1)
-plt.plot(pzt_signal)
+plt.plot(y)
 plt.title('Original signal')
 
-# plt.subplot(1, 3, 2)
-# plt.plot(pzt_denoised_wv)
-# plt.title('Wavelet denoised')
-
 plt.subplot(1, 3, 2)
-plt.plot(pzt_denoised)
-plt.title('Bandpass filtering')
+plt.plot(y_noise)
+plt.title('Noisy signal')
+
+plt.subplot(1, 3, 3)
+plt.plot(y_denoised)
+plt.title('Wavelet denoised')
 
 plt.tight_layout()
 plt.show()
+
+
